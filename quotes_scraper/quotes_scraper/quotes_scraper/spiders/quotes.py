f5e12f28d9f8445da3c66a0be12b6f6009f59b80
Fer Torres <fertorresmx@gmail.com>
2021-01-08 00:14:59 -0600

    Extract Title, Quotes and Top Ten Tags

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,5 +1,10 @@
 import scrapy
 
+# Título = //h1/a/text()
+# Citas = //span[@class="text" and @itemprop="text"]/text()
+# Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+# Next page button = //ul[@class="pager"]//li[@class="next"]/a/@href
+
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
@@ -8,7 +13,23 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         print('*' * 10)
-        print('\n\n')
-        print(response.status, response.headers)
+        print('\n')
+
+        title = response.xpath('//h1/a/text()').get()
+        print(f'Titulo: {title}')
+        print('\n')
+
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        print('Citas: ')
+        for quote in quotes:
+            print(f'- {quote}')
+        print('\n')
+
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        print('Top ten tags: ')
+        for tag in top_ten_tags:
+            print(f'- {tag}')
+        print('\n')
+
+        print('\n')
         print('*' * 10)
-        print('\n\n')
